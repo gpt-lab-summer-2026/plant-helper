2c00226366897721127d7911864990475d89b294
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2026-06-09 10:23:11 +0300

    implemented speak functionality

--- speak.py
+++ speak.py
@@ -1,1 +1,19 @@
+from piper.voice import PiperVoice
+import wave
+import subprocess
+import sys
 # ai speaks to user 
+
+# TO DO: how to select which language
+# load voice
+voice = PiperVoice.load("voices/en_GB-southern_english_female-low.onnx") # we need .onnx and .json files downloaded, for this only .onnx
+
+def speak(text):
+    # sunthesize the text to wav file 
+    with wave.open("output.wav", "wb" ) as wav_file:
+        voice.synthesize_wav(text, wav_file)
+    # play the audio
+    player = "afplay" if sys.platform == "darwin" else "aplay"
+    return subprocess.run([player, "output.wav"], check=True)
+
+speak("rainbow")
