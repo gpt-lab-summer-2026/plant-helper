ad19da4b433708bcee6ec11315dcaf514b6375ae
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2026-06-09 13:51:41 +0300

    added plant database

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from plant_profile import *
 
 # main function
 
-
 # load plant profile
 plant_profile = read()
 
@@ -19,7 +18,7 @@ def system_prompt():
         model='phi3:mini',
         messages=[{'role': 'system', 'content': f"You are a {plant_profile['species']} plant named {plant_profile['plant_name']}."},
                   {'role': 'system', 'content': f"Your soil moisture is currently {plant_profile['moisture_percentage']}. You were last watered {datetime.datetime.fromisoformat(plant_profile['last_watered'])} which was {datetime.datetime.now()-datetime.datetime.fromisoformat(plant_profile['last_watered'])} days ago."},
-                  {'role': 'system', 'content': f"Answer questions as the plant in a friendly tone and keep answers in 5 sentences."}
+                  {'role': 'system', 'content': f"Open plant_database.json file and read it and give answers based on things you read from file. Answer questions as the plant in a friendly tone and keep answers in 5 sentences. You are tired and a little grumpy when your soil is dry, and cheerful and energetic when well watered"}
                 ]           
     )
     print(response.message.content)
