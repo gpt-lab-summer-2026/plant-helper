d365de264900a470b2144b4ca517228a7b36e813
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2026-06-11 14:49:45 +0300

    added language detection

--- speak.py
+++ speak.py
@@ -7,12 +7,13 @@ import sys
 # TO DO: how to select which language
 # openclaw could detect which language
 # load voice
-voice = PiperVoice.load("voices/fi_FI-harri-low.onnx") 
+voice_fi = PiperVoice.load("voices/fi_FI-harri-low.onnx") 
+voice_en = PiperVoice.load("voices/en_GB-southern_english_female-low.onnx") 
 
 def speak(text):
     # sunthesize the text to wav file 
     with wave.open("output.wav", "wb" ) as wav_file:
-        voice.synthesize_wav(text, wav_file)
+        voice_fi.synthesize_wav(text, wav_file)
     # play the audio
     player = "afplay" if sys.platform == "darwin" else "aplay"
     return subprocess.run([player, "output.wav"], check=True)
